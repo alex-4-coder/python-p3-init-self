f1e7528e3dde476483a16c46f3cb84e537a0433a
alex kamau <alex.kamau1@student.moringaschool.com>
2025-08-29 14:41:33 -0700

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name, age=0):
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        """Increase person's age by 1."""
+        self.age += 1
+
+    def greet(self):
+        """Print a friendly greeting."""
+        print(f"Hello! My name is {self.name}.")
